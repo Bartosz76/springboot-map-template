cfb4121536efbaad985284ff36dbf1d49044fbed
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-02-12 21:35:36 +0100

    Covid19Parser class now contains the logic to parse data from an external CSV file.

---
+++ src/main/java/bm/app/springbootmaptemplate/Covid19Parser.java
@@ -0,0 +1,45 @@
+package bm.app.springbootmaptemplate;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+@Service
+public class Covid19Parser {
+
+    private static final String url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    public List<Point> getCovidData() throws IOException {
+
+        List<Point> points = new ArrayList<>();
+
+        RestTemplate restTemplate = new RestTemplate();
+        String values = restTemplate.getForObject(url, String.class);
+
+        StringReader stringReader = new StringReader(values);
+        CSVParser parse = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(stringReader);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        String currentDateAsString = dtf.format(now).toString();
+
+        for (CSVRecord strings : parse) {
+            double lat = Double.parseDouble(strings.get("Lat"));
+            double lon = Double.parseDouble(strings.get("Long"));
+            String text = strings.get("2/11/21");
+            points.add(new Point(lat, lon, text));
+        }
+        return points;
+    }
+}
