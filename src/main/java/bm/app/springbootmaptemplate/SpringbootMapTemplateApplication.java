01fc00d8c3f4aed22b99b95b9d9fb75cd605d271
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-02-12 18:18:39 +0100

    The initial commit.

---
+++ src/main/java/bm/app/springbootmaptemplate/SpringbootMapTemplateApplication.java
@@ -0,0 +1,13 @@
+package bm.app.springbootmaptemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMapTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMapTemplateApplication.class, args);
+    }
+
+}
