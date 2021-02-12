5a2ed34c86b8396f0327b7054221e192e7ba4606
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-02-12 20:10:40 +0100

    Added a Point model class and linked its object with Javascript code in HTML via Thymeleaf.

--- src/main/java/bm/app/springbootmaptemplate/MapController.java
+++ src/main/java/bm/app/springbootmaptemplate/MapController.java
@@ -1,4 +1,15 @@
 package bm.app.springbootmaptemplate;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MapController {
+
+    @GetMapping
+    public String getMap(Model model) {
+        model.addAttribute("point", new Point(52.22, 21.01, "Żarcie!"));
+        return "map";
+    }
 }
